de24413722463e176874882fbc64e02595e4e33c
Rick Sheahan <rssheah2@illinois.edu>
2016-03-26 21:52:45 -0500

    Removed firebase code. Made new data structure

--- twet.rb
+++ twet.rb
@@ -1,12 +1,5 @@
 class Twet
+  attr_accessor :owner, :name, :hatched_on, :level, :experience, :hungry, :species, :physical_trait, :personality_trait
   def initialize(name, owner)
-    @id = new_twet_id
-    @firebase.set("twet/#{@id}", {owner:owner, name:name, hatched_on: Firebase::ServerValue::TIMESTAMP,
-                                  level:1, experience:0, hungry:true})
-    @firebase.update("user/#{owner}", {twet:@id})
-  end
-
-  def hungry=(is_hungry)
-    firebase.update("twet/#{@id}")
   end
 end
