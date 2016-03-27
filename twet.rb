89c4ca94170d452790cd3622944442881af3a0de
Rick Sheahan <rssheah2@illinois.edu>
2016-03-26 22:01:36 -0500

    Implement twet initializer.

--- twet.rb
+++ twet.rb
@@ -1,5 +1,17 @@
 class Twet
-  attr_accessor :owner, :name, :hatched_on, :level, :experience, :hungry, :species, :physical_trait, :personality_trait
+  attr_accessor :owner, :name, :hatched_on, :level, :experience, :hungry, :species, :trait, :personality
   def initialize(name, owner)
+    data = YAML.load_file('world.yaml') #TODO there must be a better way to decouple this
+    @name = name
+    @owner = owner
+    @color = data['color'].sample
+    @pattern = data['pattern'].sample
+    @hatched_on = Time.now
+    @level = 1
+    @experience = 0
+    @hungry = true
+    @species = data['species'].sample
+    @trait = data['trait'].sample
+    @personality = data['personality'].sample
   end
 end
